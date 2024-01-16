0a70cd3e974fa5b8808ac6b0bda10148ccd9b01c
Abhay6606 <abhaybhadauria111pm@gmail.com>
2024-01-16 21:12:04 +0530

    FOURTH

--- app/src/main/java/com/example/isolution/Activities/ContactLeadActivity.java
+++ app/src/main/java/com/example/isolution/Activities/ContactLeadActivity.java
@@ -1,16 +1,42 @@
 package com.example.isolution.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.example.isolution.Adapter.LeadRecyclerAdapter;
 import com.example.isolution.R;
+import com.example.isolution.databinding.ActivityContactLeadBinding;
+
+import java.util.ArrayList;
 
 public class ContactLeadActivity extends AppCompatActivity {
-
+  ActivityContactLeadBinding contactLeadBinding;
+  ArrayList<GetterSetter>arrayList=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_lead);
+        contactLeadBinding=ActivityContactLeadBinding.inflate(getLayoutInflater());
+        setContentView(contactLeadBinding.getRoot());
+
+        GetterSetter pack=new GetterSetter("Abhay","Etawah","Hot,Worm,Followup","Worm");
+        arrayList.add(pack);
+        GetterSetter packk=new GetterSetter("Deep","Etawah","Hot,Worm,Followup","Hot");
+        arrayList.add(packk);
+        GetterSetter packkk=new GetterSetter("Amit","Etawah","Hot,Worm,Followup","Follow up");
+        arrayList.add(packkk);
+
+
+
+
+        contactLeadBinding.recyclerViewdetails.setLayoutManager(new LinearLayoutManager(this));
+        LeadRecyclerAdapter adapter=new LeadRecyclerAdapter(this,arrayList);
+        contactLeadBinding.recyclerViewdetails.setAdapter(adapter);
+
+
+
+
+
     }
 }
