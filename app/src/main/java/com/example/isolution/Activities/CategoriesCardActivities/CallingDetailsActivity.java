96d3039cd1427b4b6f6b4a41cf05daecaa34b9a2
Abhay6606 <abhaybhadauria111pm@gmail.com>
2024-02-15 23:14:37 -0800

    yoyo honey singh

--- app/src/main/java/com/example/isolution/Activities/CategoriesCardActivities/CallingDetailsActivity.java
+++ app/src/main/java/com/example/isolution/Activities/CategoriesCardActivities/CallingDetailsActivity.java
@@ -41,6 +41,7 @@ public class CallingDetailsActivity extends AppCompatActivity {
         setContentView(callingDetailsBinding.getRoot());
         askPermission();
 
+
 //        CallingDetailsGetterSetter pack = new CallingDetailsGetterSetter("https://instagram.fdel27-5.fna.fbcdn.net/v/t51.2885-19/269382591_431678785170231_3739845402388046538_n.jpg?stp=dst-jpg_s150x150&efg=e30&_nc_ht=instagram.fdel27-5.fna.fbcdn.net&_nc_cat=103&_nc_ohc=2f-l3xXFQsQAX-1yEGN&edm=ABmJApABAAAA&ccb=7-5&oh=00_AfDyr8nPqNe05uvvv2TCUZ3wk-ob5pZKi3byudyAQ2txtA&oe=65B80EAD&_nc_sid=b41fef", "Abhay Bhadauria", "6398960435", "23m12s");
 //        arrayList.add(pack);
 
@@ -165,3 +166,4 @@ public class CallingDetailsActivity extends AppCompatActivity {
         return logListArrayList;
     }
 }
+//====
